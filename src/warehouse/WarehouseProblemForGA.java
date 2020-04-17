a9413d642f9f55d02b645a7a9ae04a9df9632570
andresc3 <2181858@my.ipleiria.pt>
2020-04-17 16:28:20 +0100

    WharehouseProblemForGA

--- src/warehouse/WarehouseProblemForGA.java
+++ src/warehouse/WarehouseProblemForGA.java
@@ -2,27 +2,40 @@ package warehouse;
 
 import ga.Problem;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class WarehouseProblemForGA implements Problem<WarehouseIndividual> {
 
     //TODO this class might require the definition of additional methods and/or attributes
+    private static LinkedList<Cell> shelves;
+    private static Cell cellAgent;
+    private static Cell exit;
+    private static ArrayList<Request> requests;
+    private static int numProducts;
+    private static LinkedList<Pair> pairs;
 
     public WarehouseProblemForGA(WarehouseAgentSearch agentSearch) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        shelves = WarehouseAgentSearch.getShelves();
+        cellAgent = WarehouseAgentSearch.getCellAgent();
+        exit = WarehouseAgentSearch.getExit();
+        requests = WarehouseAgentSearch.getRequests();
+        numProducts = WarehouseAgentSearch.getNumProducts();
+        pairs = agentSearch.getPairs();
+
     }
 
     @Override
     public WarehouseIndividual getNewIndividual() {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return new WarehouseIndividual(this, shelves.size());
     }
 
     @Override
     public String toString() {
         //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+
+        return "Alguritmo genetico";
     }
 
 }
