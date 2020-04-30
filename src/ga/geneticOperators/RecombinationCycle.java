0c36df218e9187c32ee01aa0497a7f51c03d9993
andresc3 <2181858@my.ipleiria.pt>
2020-04-30 16:14:33 +0100

    Inversion Mutation Total Work

--- src/ga/geneticOperators/RecombinationCycle.java
+++ src/ga/geneticOperators/RecombinationCycle.java
@@ -13,7 +13,7 @@ public class RecombinationCycle<I extends IntVectorIndividual, P extends Problem
         super(probability);
     }
 
-    private int[] child1,child2;
+    private int[] child1,child2,parent1,parent2;
 
     private int[] visitedIndices;
     private int visitedIndicesPos;
@@ -24,12 +24,18 @@ public class RecombinationCycle<I extends IntVectorIndividual, P extends Problem
     @Override
     public void recombine(I ind1, I ind2) {
         child1 = ind1.getGenome();
+        parent1 = ind1.getGenome();
+        System.out.println("Parent1 antes");
+        System.out.println(Arrays.toString(parent1));
         child2 = ind2.getGenome();
+        parent2 = ind2.getGenome();
+        System.out.println("Parent2 antes");
+        System.out.println(Arrays.toString(parent2));
         indice = new int[ind1.getNumGenes()];
         Arrays.fill(indice, -1);
         visitedIndices = new int[ind1.getNumGenes()];
         Arrays.fill(visitedIndices, -1);
-
+        visitedIndicesPos = 0;
         int cycle = 1;
         int length = child1.length;
         int idx = 0;
@@ -45,16 +51,22 @@ public class RecombinationCycle<I extends IntVectorIndividual, P extends Problem
                 item = child1[idx];
                 idx = getPos(child2,item);
             }
-            if(cycle++ % 2 != 0){
-                for (int i: indice) {
-                    tmp = child2[i];
-                    ind1.setGene(i, child1[i]);
-                    ind2.setGene(i,tmp);
+
+                if (cycle++ % 2 != 0) {
+                    for (int i : indice) {
+                        if (i != -1) {
+                            tmp = child2[i];
+                            parent2[i] = child1[i];
+                            ind1.setGene(i, child1[i]);
+                            ind2.setGene(i, tmp);
+                            parent1[i] = tmp;
+                        }
+                    }
                 }
-            }
-            insertVisited(indice);
+                insertVisited(indice);
+
             idx = (indice[0]+1)% length;
-            while (verifyVisited(idx) && visitedIndices.length < length){
+            while (verifyVisited(idx) && lengthVisitedIndices() < length){
                 idx++;
                 if (idx>=length){
                     idx = 0;
@@ -62,24 +74,31 @@ public class RecombinationCycle<I extends IntVectorIndividual, P extends Problem
             }
             clearIndice();
         }
+        System.out.println("Parent1 depois");
+        System.out.println(Arrays.toString(parent1));
+        System.out.println("Parent2 depois");
+        System.out.println(Arrays.toString(parent2));
     }
 
     private int lengthVisitedIndices() {
         int i = 0;
         for (int e:visitedIndices) {
-            i++;
+            if (e!=-1){
+                i++;
+            }
         }
         return i;
     }
 
     private void insertVisited(int[] indice) {
         for (int i: indice) {
+            if (i!=-1)
             visitedIndices[visitedIndicesPos++] = i;
         }
     }
 
     private void clearIndice() {
-        Arrays.fill(indice,0);
+        Arrays.fill(indice,-1);
         indiceNextPos=0;
     }
 
